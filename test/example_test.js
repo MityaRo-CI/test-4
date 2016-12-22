fa025e02812be54e212283d1ffa1e4d1203ce40f
MityaRo-CI <mityaro.ci@gmail.com>
2016-12-22 10:24:16 +0200

    Update example_test.js

--- test/example_test.js
+++ test/example_test.js
@@ -3,13 +3,11 @@ var assert = require('assert')
 var should = require('should');
 
 var branchName = process.env.TRAVIS_BRANCH || 'http://myurl.com'
-var branchUrl = 'http://ci-workshop-test-1-' + branchName + '.herokuapp.com'
+var branchUrl = 'http://ci-wksh-test-4-' + branchName + '.herokuapp.com'
     
-describe('x', function() {
-  describe('1', function() {
-    it('Title', function() {
+describe('Home page tests', function() {
+    it('Header is correct', function() {
         browser.url(branchUrl)
         browser.getText('h1').should.equal('My Site')
     });
-  });
 });
